331a83e1a86afb93dcbef227fe81634723e9ab7b
Plato Manchi <onelord2002@gmail.com>
2025-04-26 19:30:30 +0530

    Adding ISPC support for ROC

--- TradinatorCore/Private/Indicators/ROC.cpp
+++ TradinatorCore/Private/Indicators/ROC.cpp
@@ -7,7 +7,7 @@
 
 #include "indicator_helper_ispc.h"
 
-#if 0
+#if 1
 #define _ROC_ISPC_
 #else
 #endif
@@ -28,12 +28,35 @@ std::vector<IndicatorPoint> ROC::Calculate()
 			is_ready = candle_data->IsDataReady();
 		}
 
-		//std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+		std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
 
 		size_t count = candle_data->GetData().size();
 		result.reserve(count);
 
 #ifdef _ROC_ISPC_
+		const AsyncCandleData& data = candle_data->GetData();
+		std::vector<double> ispc_input;
+		std::vector<double> ispc_output(count);
+		ispc_input.reserve(count);
+
+		for (auto& pair : data)
+		{
+			ispc_input.emplace_back(pair.second.m_close);
+		}
+
+		ispc::calculate_roc(ispc_input.data(), ispc_output.data(), count, m_length);
+
+		auto itr = candle_data->GetData().begin();
+		for (double sma : ispc_output)
+		{
+			IndicatorPoint point;
+			point.date = (*itr).first;
+			point.value = sma;
+
+			result.emplace_back(std::move(point));
+
+			std::advance(itr, 1);
+		}
 #else
 		auto itr = candle_data->GetData().begin();
 		auto end_itr = candle_data->GetData().end();
@@ -58,8 +81,8 @@ std::vector<IndicatorPoint> ROC::Calculate()
 #endif // _ROC_ISPC_
 
 
-		//std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-		//std::cout << "ROC Took " << std::to_string(std::chrono::duration<double>(end - start).count()) << " sec" << std::endl;
+		std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+		std::cout << "ROC Took " << std::to_string(std::chrono::duration<double>(end - start).count()) << " sec" << std::endl;
 	}
 
 	return result;
